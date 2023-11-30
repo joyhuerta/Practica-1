4ee986c0d610513225ec209b6ff50b507f403e71
joyhuerta <147471827+joyhuerta@users.noreply.github.com>
2023-11-29 23:40:42 -0600

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+celcius = np.array([-40,-10,0,8,15,22,38], dtype=float)
+fahrenheit = np.array([-40,-10,0,8,15,22,38], dtype=float)
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1,oculta2,salida])
+
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.01),
+    loss= 'mean_squared_error'
+)
+
+print("Entrenando . . .")
+historial = modelo.fit(celcius, fahrenheit, epochs=300, verbose=False)
+print("¡Modelo entrenado!")
+
+import matplotlib.pyplot as plt
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de Pérdida")
+plt.plot(historial.history["loss"])
+
+print("Realiza una predicción")
+resultado = modelo.predict([100,00])
+print("El resultado es:"+str(resultado)+"°F")
+
+modelo.save('celcius_a_fahrenheit.h5')
+
